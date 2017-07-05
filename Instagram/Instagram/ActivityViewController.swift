7f7fe0f04021ffd523b0eeb642d5cfe82e1d90c7
Chidi Emeh <chidiemeh184@gmail.com>
2017-07-05 16:21:40 -0400

    Login Sign In VC

---
+++ Instagram/Instagram/ActivityViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ActivityViewController.swift
+//  Instagram
+//
+//  Created by Chidi Emeh on 7/5/17.
+//  Copyright © 2017 Chilly Bean. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Instagram/Instagram/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  Instagram
+//
+//  Created by Chidi Emeh on 7/5/17.
+//  Copyright © 2017 Chilly Bean. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
